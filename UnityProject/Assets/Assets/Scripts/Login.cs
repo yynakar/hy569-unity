b393ab2a1b788a65402c26925ff9a113f6fa6eba
categerjsy <katerinagerak99@gmail.com>
2022-06-03 12:37:53 +0300

    Login with database

--- UnityProject/Assets/Assets/Scripts/Login.cs
+++ UnityProject/Assets/Assets/Scripts/Login.cs
@@ -3,38 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using System.Net.Http;
 public class Login : MonoBehaviour
 {
     public InputField inputUsername;
     public InputField inputPassword;
     public Text response;
-    private static readonly HttpClient client = new HttpClient();
-
     IEnumerator ieLogin()
     {
         WWWForm dataForm = new WWWForm();
         dataForm.AddField("userName", inputUsername.text);
         dataForm.AddField("passWord", inputPassword.text);
-        string uri = "http://arthunt.epizy.com/Login.php";
-        // System.Net.Http.HttpClient
-        //HttpClient
-        //System.Net.Http.HttpClient webRequest = new System.Net.Http.HttpClient();
-        //webRequest.PostAsync(uri, dataForm);
-        //webRequest.chunkedTransfer = false;
-        var values = new Dictionary<string, string>
-          {
-              { "userName", inputUsername.text },
-              { "passWord", inputPassword.text }
-          };
-        var content = new FormUrlEncodedContent(values);
-        var response = await client.PostAsync("http://arthunt.epizy.com/Login.php", content);
+        string uri = "https://arthunt.000webhostapp.com/Login.php";
+
+        //string uri = "http://localhost/CS569-WebApplication/htdocs/Login.php";
+        UnityWebRequest webRequest = UnityWebRequest.Post(uri, dataForm);
+        webRequest.chunkedTransfer = false;
 
         yield return webRequest.SendWebRequest();
 
         //Debug.Log(webRequest.downloadHandler.text);
-        //response.text = "Res: " + webRequest.downloadHandler.text +".";
-        response.text = webRequest.error;
+        response.text = "Res: " + webRequest.downloadHandler.text + ".";
 
     }
 
