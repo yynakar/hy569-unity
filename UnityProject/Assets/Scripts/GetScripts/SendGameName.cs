ff05b61cf6a361dab5b2615dd7b1bb637b5f345b
categerjsy <katerinagerak99@gmail.com>
2022-06-19 19:14:37 +0300

    correction

--- UnityProject/Assets/Scripts/GetScripts/SendGameName.cs
+++ UnityProject/Assets/Scripts/GetScripts/SendGameName.cs
@@ -50,7 +50,7 @@ public class SendGameName : MonoBehaviour
         }
         GameObject.Find("DataManager").GetComponent<DataManagement>().TreasureHuntID = int.Parse(values[1]);
         GameObject.Find("DataManager").GetComponent<DataManagement>().TeamName = values[3];
-        GameObject.Find("Dashboard(Clone)/UI/Canvas/Responses/Team Name").GetComponent<TextMeshProUGUI>().text = values[3];
+        GameObject.Find("Dashboard(Clone)/UI/Canvas/BluePanel/Responses/Team Name").GetComponent<TextMeshProUGUI>().text = values[3];
         gameObject.transform.parent.gameObject.SetActive(false);
 
        // GameObject.Find("DataManager").GetComponent<DataManagement>().TreasureHuntID = int.Parse(values[1]);
