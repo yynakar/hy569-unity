c5492aae7786f57b3367b69ef370878dc63e23d4
categerjsy <katerinagerak99@gmail.com>
2022-06-21 17:02:03 +0300

    riddle moving

--- UnityProject/Assets/Scripts/ARAppScripts/GoDashboard.cs
+++ UnityProject/Assets/Scripts/ARAppScripts/GoDashboard.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Linq;
+using TMPro;
+
 
 public class GoDashboard : MonoBehaviour
 {
     public GameObject Dashboard;
-    // public GameObject ARFoundation;
+    public ArrayList riddles;
     public void GoToDashboard()
     {
         Instantiate(Resources.Load("Prefabs/Dashboard"));
-        //gameObject.SetActive(false);
+
+        riddles = GameObject.Find("DataManager").GetComponent<DataManagement>().Riddles;
+        riddles.RemoveAt(0);
+
+        GameObject.Find("DataManager").GetComponent<DataManagement>().Riddles = riddles;
+        Riddle r = (Riddle)riddles[0];
+        string FirstRiddle = r.getText();
+        Debug.Log("First Text" + FirstRiddle);
+        GameObject.Find("Dashboard(Clone)/UI/Canvas/BluePanel/Responses/Riddle").GetComponent<TextMeshProUGUI>().text = FirstRiddle;
+        string FirstInfo = r.getInfo();
+        GameObject.Find("Solved(Clone)/UI/Canvas/BluePanel/Responses/InfoText").GetComponent<TextMeshProUGUI>().text = FirstInfo;
+
+
+
     }
 }
